e3e82efcc27bd3575db6ddcb89c4a96b0a3bc914
dem4ply <dem4ply@gmail.com>
2015-02-16 05:40:26 -0600

    inicio de tp_camera

---
+++ Assets/_script/controllers/camera/Third_person_camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Controller{
+	namespace Eye {
+		public class Third_person_camera : MonoBehaviour {
+
+			void Start() {
+			}
+
+			void Update() {
+			}
+		}
+	}
+}

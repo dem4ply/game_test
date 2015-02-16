ff53ce37e3080cf80ad25fa0f1bf38f2248dc14f
dem4ply <dem4ply@gmail.com>
2015-02-16 04:14:53 -0600

    se inicio la nueva vercion del controller

---
+++ Assets/_script/controllers/controllers/Controller_base.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Controller.Motor;
+
+namespace Controller {
+	namespace Controller {
+		public class Controller_base : MonoBehaviour {
+			protected Transform _transform;
+			protected Motor_base _motor;
+
+			protected void Awake() {
+			}
+
+			protected void Update() {
+				if ( Camera.main == null ) {
+					return;
+				}
+			}
+
+			/// <summary>
+			/// inicializa el chache del script
+			/// </summary>
+			protected virtual void _init_cache() {
+				_transform = transform;
+				_motor = GetComponent<Motor_base>();
+			}
+		}
+	}
+}
+
---
+++ Assets/_script/controllers/motors/Motor_base.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Controller {
+	namespace Motor {
+		public class Motor_base : MonoBehaviour {
+			protected Transform _transform;
+
+			protected void Awake() {
+			}
+
+			protected void Update() {
+			}
+
+			/// <summary>
+			/// inicializa el chache del script
+			/// </summary>
+			protected virtual void _init_cache() {
+				_transform = transform;
+			}
+		}
+	}
+}
